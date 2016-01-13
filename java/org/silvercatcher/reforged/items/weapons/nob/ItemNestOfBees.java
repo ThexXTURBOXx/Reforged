f9b4cc07f6f1ca4d7d23881d4c0a619dc9219d99
Joel Lacour <joelgo.lacour@googlemail.com>
2016-01-13 16:54:01 +0200

    making progress with nest of bees crafting

--- java/org/silvercatcher/reforged/items/weapons/nob/ItemNestOfBees.java
+++ java/org/silvercatcher/reforged/items/weapons/nob/ItemNestOfBees.java
@@ -31,7 +31,10 @@ public class ItemNestOfBees extends ReforgedItem {
 	
 	
 	public ItemNestOfBees() {
+		
 		super("nest_of_bees");
+		setMaxDamage(80);
+		setMaxStackSize(1);
 	}
 	
 	
@@ -125,127 +128,14 @@ public class ItemNestOfBees extends ReforgedItem {
 		}
         world.playSoundAtEntity(shooter, "fireworks.launch", 3.0f, 1.0f);
 	}
-	
-	/*
-	public static final int ROUND_DELAY = 8;
-	public static final float INACCUARY = 3f;
-	public static final float DAMAGE = 2.5f;
-	public static final float ARROW_SPEED = 1.75f;
 
-	public static final int MAX_AMMO = 32;
-	
-	private LoadStates loadState;
-	
-	public ItemNestOfBees() {
+	public int getAmmoCount(NBTTagCompound compound) {
 		
-		super("nest_of_bees");
-		loadState = LoadStates.EMPTY;
-		setMaxStackSize(1);
-		setMaxDamage(100);
-	}
-
-
-	
-	@Override
-	public void registerRecipes() {
-		
-		ItemStack emptyNoB = new ItemStack(this, );
-		
-		GameRegistry.a
-		GameRegistry.addShapedRecipe(emptyNoB,
-				"slw",
-				"slw",
-				"slw",
-				's', Items.string,
-				'l', Items.leather,
-				'w', Items.stick);
+		return compound.getInteger(CompoundTags.AMMUNITION);
 	}
 	
-
-	
-	@Override
-	public EnumAction getItemUseAction(ItemStack stack) {
+	public int getAmmoCount(ItemStack stack) {
 		
-		if(loadState == LoadStates.RELOADING) return EnumAction.BLOCK;
-		if(loadState == LoadStates.LOADED) return EnumAction.BOW;
-		return EnumAction.NONE;
+		return getAmmoCount(giveCompound(stack));
 	}
-	
-	@Override
-	public ItemStack onItemRightClick(ItemStack itemStackIn, World worldIn, EntityPlayer playerIn) {
-		
-		itemStackIn.onCrafting(worldIn, playerIn, MAX_AMMO);
-		NBTTagCompound nbt = initReloadTags(itemStackIn);
-
-		if(playerIn.isSneaking()) {
-			
-			int loadedArrows = nbt.getInteger(CompoundTags.AMMUNITION);
-			
-			if(loadedArrows < MAX_AMMO && playerIn.inventory.consumeInventoryItem(Items.arrow)) {
-				nbt.setInteger(CompoundTags.AMMUNITION, loadedArrows + 1);
-			}
-		}		
-		
-		int loadedCatalyst = nbt.getInteger(CompoundTags.CATALYST);
-		
-		// no catalyst
-		if(loadedCatalyst == 0) {
-			
-			// check for ammunition
-			if(playerIn.inventory.consumeInventoryItem(Items.gunpowder)) {
-				playerIn.setItemInUse(itemStackIn, getReloadTotal());
-				loadState = LoadStates.RELOADING;
-				nbt.setInteger(CompoundTags.CATALYST, 32);
-			} else {
-				playerIn.setItemInUse(itemStackIn, getMaxItemUseDuration(itemStackIn));
-			}
-			// ready to fire
-		} else {
-			//System.out.println("Let's boom!");
-			loadState = LoadStates.LOADED;
-			playerIn.setItemInUse(itemStackIn, ROUND_DELAY);
-		}
-		return itemStackIn;
-	}
-	
-	@Override
-	public ItemStack onItemUseFinish(ItemStack stack, World worldIn, EntityPlayer playerIn) {
-
-		if(loadState == LoadStates.EMPTY) return stack;
-		
-		NBTTagCompound nbt = stack.getTagCompound();
-
-		if(loadState == LoadStates.LOADED) {
-			int count = 0;
-			while(count++ < 4) {
-				shoot(worldIn, playerIn);
-			}
-			stack.getTagCompound().setLong(CompoundTags.RELOAD, worldIn.getTotalWorldTime());
-			stack.damageItem(1, playerIn);
-			nbt.setInteger(CompoundTags.RELOAD, getReloadTotal());
-			loadState = LoadStates.EMPTY;
-		} else if (loadState == LoadStates.RELOADING) {
-			nbt.setInteger(CompoundTags.RELOAD, 0);
-			loadState = LoadStates.LOADED;
-		}
-		return stack;
-	}
-	
-	@Override
-	@SideOnly(Side.CLIENT)
-	public void addInformation(ItemStack stack, EntityPlayer playerIn,
-			@SuppressWarnings("rawtypes") List tooltip, boolean advanced) {
-		
-		NBTTagCompound nbt = stack.getTagCompound();
-		if(nbt != null) {
-			tooltip.add("Arrows: " + nbt.getInteger(CompoundTags.AMMUNITION));
-			tooltip.add("Gunpowder: " + nbt.getInteger(CompoundTags.CATALYST));
-		}
-	}
-
-	@Override
-	public float getHitDamage() {
-		return 1f;
-	}
-	*/
 }
--- java/org/silvercatcher/reforged/items/weapons/nob/NestOfBeesLoadRecipe.java
+++ java/org/silvercatcher/reforged/items/weapons/nob/NestOfBeesLoadRecipe.java
@@ -1,40 +1,106 @@
 package org.silvercatcher.reforged.items.weapons.nob;
 
-import org.silvercatcher.reforged.ReforgedItems;
+import java.util.LinkedList;
 
+import org.silvercatcher.reforged.ReforgedItems;
+import org.silvercatcher.reforged.items.CompoundTags;
+
+import net.minecraft.client.Minecraft;
 import net.minecraft.init.Items;
 import net.minecraft.inventory.InventoryCrafting;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.IRecipe;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.world.World;
 import net.minecraftforge.common.ForgeHooks;
 
 public class NestOfBeesLoadRecipe implements IRecipe {
 
 	private ItemStack output;
-	private ItemStack [] input;
+	private ItemStack input [];
+	
+	private static void printInventory(String name, InventoryCrafting inventory) {
+	
+		if(Minecraft.getMinecraft().theWorld != null) {
+			
+			System.out.append(name);
+			System.out.append(":\t[");
+			for(int i = 0; i < inventory.getSizeInventory(); i++) {
+				System.out.append(inventory.getStackInSlot(i) + ",");
+			}
+			System.out.append("]");
+			System.out.println();
+		}
+	}
 	
 	@Override
 	public boolean matches(InventoryCrafting inventory, World world) {
+		
+		printInventory("match", inventory);
+		
+		int nestsOfBees = 0;
+		int arrowBundles = 1;
 		
 		for(int i = 0; i < inventory.getSizeInventory(); i++) {
 			
 			ItemStack stack = inventory.getStackInSlot(i);
 			
-			if(stack != null && stack.getItem() == ReforgedItems.NEST_OF_BEES) {
-				return true;
+			if(stack != null) {
+				if(stack.getItem() == ReforgedItems.NEST_OF_BEES) {
+					nestsOfBees++;
+				} else if(stack.getItem() == ReforgedItems.ARROW_BUNDLE) {
+					arrowBundles++;
+				} else {
+					// we don't want any other stuff!
+					return false;
+				}
 			}
 		}
-		return false;
+		
+		return nestsOfBees == 1 && arrowBundles > 0;
 	}
 
 	@Override
 	public ItemStack getCraftingResult(InventoryCrafting inventory) {
 
+		printInventory("result", inventory);
 		
-		output = new ItemStack(ReforgedItems.NEST_OF_BEES);
+		int size = inventory.getSizeInventory();
+		// we can assume that the ingredients match, at least one NoB has to be there...I hope
+		ItemStack nestOfBees = null;
+		
+		// remember where to get arrow bundles....better idea?
+		LinkedList<Integer> arrowBundleIndizes = new LinkedList<Integer>();
+		
+		for(int i = 0; i < size; i++) {
+			
+			ItemStack stack = inventory.getStackInSlot(i);
+			
+			if(stack != null) {
+				Item item = stack.getItem();
+				if(item == ReforgedItems.NEST_OF_BEES) {
+					nestOfBees = stack;
+				} else if(item == ReforgedItems.ARROW_BUNDLE) {
+					arrowBundleIndizes.add(i);
+				}
+			}
+		}
+		
+		NBTTagCompound compound = ReforgedItems.NEST_OF_BEES.giveCompound(nestOfBees);
+		
+		int arrows = compound.getInteger(CompoundTags.AMMUNITION);
+		
+		// please terminate, please terminate,...
+		while(!arrowBundleIndizes.isEmpty() && arrows < 32 - 7) {
+			arrows += 8;
+			input[arrowBundleIndizes.removeFirst()] = null;
+		}
+		
+		compound.setInteger(CompoundTags.AMMUNITION, arrows);
+		input = new ItemStack[inventory.getSizeInventory()];
 
-		return output;
+		return nestOfBees;
 	}
 
 	@Override
@@ -45,20 +111,15 @@ public class NestOfBeesLoadRecipe implements IRecipe {
 
 	@Override
 	public ItemStack getRecipeOutput() {
+		
 		return output;
 	}
 
 	@Override
 	public ItemStack[] getRemainingItems(InventoryCrafting inventory) {
 		
-		ItemStack [] remaining = new ItemStack[inventory.getSizeInventory()];
+		printInventory("remain", inventory);
 		
-		for(int i = 0; i < remaining.length; i++) {
-			
-			if(input[i] != output) {
-				remaining[i] = input[i];
-			}
-		}
-		return remaining;
+		return input;
 	}
 }
