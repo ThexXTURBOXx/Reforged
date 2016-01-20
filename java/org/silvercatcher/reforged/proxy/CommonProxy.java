da1b1613bd3ae313fffce920d8d069b2c3cb0f77
ThexXTURBOXx <nico.mexis@kabelmail.de>
2016-01-20 17:40:23 +0100

    Project cleanup

--- java/org/silvercatcher/reforged/proxy/CommonProxy.java
+++ java/org/silvercatcher/reforged/proxy/CommonProxy.java
@@ -2,18 +2,13 @@ package org.silvercatcher.reforged.proxy;
 
 import org.silvercatcher.reforged.ReforgedEvents;
 import org.silvercatcher.reforged.ReforgedMod;
-import org.silvercatcher.reforged.ReforgedMonsterArmourer;
 import org.silvercatcher.reforged.ReforgedRegistry;
 import org.silvercatcher.reforged.entities.EntityBoomerang;
 import org.silvercatcher.reforged.entities.EntityBulletMusket;
 import org.silvercatcher.reforged.entities.EntityJavelin;
 
 import net.minecraft.client.renderer.entity.RenderManager;
-import net.minecraft.item.Item.ToolMaterial;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.config.Configuration;
-import net.minecraftforge.common.config.Property;
-import net.minecraftforge.common.util.EnumHelper;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLInterModComms;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
