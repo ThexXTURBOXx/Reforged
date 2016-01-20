70a0d26056eaac43b349a85a1f7cdcdb9ac4ff41
Joel Lacour <joelgo.lacour@googlemail.com>
2016-01-21 00:43:43 +0200

    added throwername

--- java/org/silvercatcher/reforged/entities/EntityBoomerang.java
+++ java/org/silvercatcher/reforged/entities/EntityBoomerang.java
@@ -43,11 +43,14 @@ public class EntityBoomerang extends EntityThrowable {
 	@Override
 	protected void entityInit() {
 	
+		
 		super.entityInit();
 		
 		// id 5 = ItemStack of Boomerang, type 5 = ItemStack
 		dataWatcher.addObjectByDataType(5, 5);
 
+		// id 6 = Name of Thrower, type 4 = String
+		dataWatcher.addObjectByDataType(6, 4);;
 	}
 
 	public ItemStack getItemStack() {
@@ -63,6 +66,16 @@ public class EntityBoomerang extends EntityThrowable {
 		dataWatcher.updateObject(5, stack);
 	}
 	
+	public String getThrowerName() {
+		
+		return dataWatcher.getWatchableObjectString(6);
+	}
+	
+	public void setThrowerName(String name) {
+		
+		dataWatcher.updateObject(6, name);
+	}
+	
 	private void printDatawatcher() {
 		
 		System.out.println("##########");
@@ -70,6 +83,7 @@ public class EntityBoomerang extends EntityThrowable {
 			WatchableObject wo = (WatchableObject) o;
 			System.out.println(wo.getDataValueId() + ": (" + wo.getObjectType() + ") " + wo.getObject());
 		}
+		
 		System.out.println("++++++++++++++++");
 	}
 	
@@ -138,6 +152,8 @@ public class EntityBoomerang extends EntityThrowable {
 	public void writeEntityToNBT(NBTTagCompound tagCompound) {
 		
 		super.writeEntityToNBT(tagCompound);
+		
+		tagCompound.setTag("thrower", getThrower().getEntityData());
 		
 		if(getItemStack() != null) {
 			tagCompound.setTag("item", getItemStack().writeToNBT(new NBTTagCompound()));
