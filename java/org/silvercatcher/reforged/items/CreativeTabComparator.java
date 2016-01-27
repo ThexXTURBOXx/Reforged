9349e0426f27d49e8271cf1c1142e355aeda07e2
ThexXTURBOXx <nico.mexis@kabelmail.de>
2016-01-27 20:34:56 +0100

    Added Comparator (doesn't work yet...)

---
+++ java/org/silvercatcher/reforged/items/CreativeTabComparator.java
@@ -0,0 +1,15 @@
+package org.silvercatcher.reforged.items;
+
+import java.util.Comparator;
+
+import org.silvercatcher.reforged.ReforgedRegistry;
+
+import net.minecraft.item.ItemStack;
+
+public class CreativeTabComparator implements Comparator<ItemStack> {
+
+	@Override
+	public int compare(ItemStack arg0, ItemStack arg1) {
+		return 0;
+	}
+}
