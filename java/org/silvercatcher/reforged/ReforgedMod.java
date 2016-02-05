4197566d29eaa08c22ab6bbfca3b9519c1743de3
ThexXTURBOXx <nico.mexis@kabelmail.de>
2016-02-05 14:08:50 +0100

    Bump Version

--- java/org/silvercatcher/reforged/ReforgedMod.java
+++ java/org/silvercatcher/reforged/ReforgedMod.java
@@ -16,7 +16,7 @@ public class ReforgedMod
 {
 	public static final String NAME = "Reforged";
     public static final String ID = "reforged";
-    public static final String VERSION = "0.5.9";
+    public static final String VERSION = "0.5.9-hotfix1";
     
     public static final CreativeTabs tabReforged = new CreativeTabs(ID) {
 		
