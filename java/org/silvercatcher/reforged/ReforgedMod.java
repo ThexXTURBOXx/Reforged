15257312e1638c9e01641f3616c8aff20148a83a
ThexXTURBOXx <nico.mexis@kabelmail.de>
2016-02-11 00:08:23 +0100

    Fixed spelling error

--- java/org/silvercatcher/reforged/ReforgedMod.java
+++ java/org/silvercatcher/reforged/ReforgedMod.java
@@ -19,7 +19,7 @@ public class ReforgedMod
     public static final String VERSION = "0.6-pre1";
     //Value depends on Download location... If the update is only released
     //on GitHub, then set this to true
-    //If it gets released on CurseForge aswell, then set it to false
+    //If it gets released on CurseForge as well, then set it to false
     public static final boolean BETA = true;
     
     public static final CreativeTabs tabReforged = new CreativeTabs(ID) {
