218ddf0f739f3b619ba15e5f3e38e5ab149a8539
ThexXTURBOXx <nico.mexis@kabelmail.de>
2016-02-11 17:59:58 +0100

    Added another description

--- java/org/silvercatcher/reforged/models/ReforgedModel.java
+++ java/org/silvercatcher/reforged/models/ReforgedModel.java
@@ -10,6 +10,12 @@ public class ReforgedModel extends ModelBase {
         textureHeight = 32;
 	}
 	
+	/** This was a helper method of Tabula. Doesn't work, but I let it stay here for later... 
+	 * @param modelRenderer a instance of {@link ModelRenderer}.
+	 * @param x motion in x-axis
+	 * @param y motion in y-axis
+	 * @param z motion in z-axis
+	 */
     public void setRotateAngle(ModelRenderer modelRenderer, float x, float y, float z) {
         modelRenderer.rotateAngleX = x;
         modelRenderer.rotateAngleY = y;
