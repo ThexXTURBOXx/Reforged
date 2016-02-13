b3975b83fc905dc2e7d5653db7e3f8bce2af1ad3
ThexXTURBOXx <nico.mexis@kabelmail.de>
2016-02-13 23:09:08 +0100

    Fixed Knife not getting damage when attacking enemies in Survival

--- java/org/silvercatcher/reforged/items/weapons/ItemKnife.java
+++ java/org/silvercatcher/reforged/items/weapons/ItemKnife.java
@@ -8,7 +8,7 @@ import org.silvercatcher.reforged.material.MaterialManager;
 import com.google.common.collect.Multimap;
 
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
@@ -18,7 +18,7 @@ import net.minecraft.util.Vec3;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class ItemKnife extends ItemSword implements ItemExtension {
-
+	
 	protected final MaterialDefinition materialDefinition;
 	
 	public ItemKnife(ToolMaterial material) {
@@ -36,40 +36,39 @@ public class ItemKnife extends ItemSword implements ItemExtension {
 	@Override
 	public boolean onLeftClickEntity(ItemStack stack, EntityPlayer player, Entity entity) {
 		
-		boolean hit = super.onLeftClickEntity(stack, player, entity);
-		
-		if(entity instanceof EntityLiving) {
+		if(!super.onLeftClickEntity(stack, player, entity)) {
 			
-			EntityLiving target = (EntityLiving) entity;
-			
-			Vec3 look = target.getLookVec();
-			Vec3 attacker = new Vec3(player.posX - target.posX,
-					(player.getEntityBoundingBox().minY + player.height / 2)
-					- target.posY + target.getEyeHeight(),
-					player.posZ - target.posZ);
-			double d0 = attacker.lengthVector();
-
-			double d1 = look.dotProduct(attacker);
-
-			boolean seen = d1 > 1 - 0.25 / d0;
-						
-			if(!seen && target.canEntityBeSeen(player)) {
-				target.attackEntityFrom(DamageSource.causePlayerDamage(player), getHitDamage() + 2f);
-			} else {
-				target.attackEntityFrom(DamageSource.causePlayerDamage(player), getHitDamage());
+			if(entity instanceof EntityLivingBase) {
+				
+				EntityLivingBase target = (EntityLivingBase) entity;
+				
+				Vec3 look = target.getLookVec();
+				Vec3 attacker = new Vec3(player.posX - target.posX,
+						(player.getEntityBoundingBox().minY + player.height / 2)
+						- target.posY + target.getEyeHeight(),
+						player.posZ - target.posZ);
+				double d0 = attacker.lengthVector();
+				
+				double d1 = look.dotProduct(attacker);
+				
+				boolean seen = d1 > 1 - 0.25 / d0;
+				
+				if(!seen && target.canEntityBeSeen(player)) {
+					target.attackEntityFrom(DamageSource.causePlayerDamage(player), getHitDamage() + 2f);
+				} else {
+					target.attackEntityFrom(DamageSource.causePlayerDamage(player), getHitDamage());
+				}
 			}
 		}
-		
-		return true;
+		return false;
 	}
 	
 	@Override
 	public void registerRecipes() {
-		
 		GameRegistry.addShapelessRecipe(new ItemStack(this),
 				new ItemStack(Items.stick), materialDefinition.getRepairMaterial());
 	}
-
+	
 	@Override
 	public float getHitDamage() {
 		return materialDefinition.getDamageVsEntity() + 2f;
