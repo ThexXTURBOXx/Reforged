2a891e84429f744113e9193d17f9599923966f85
Nico <nico.mexis@kabelmail.de>
2016-02-15 22:04:46 +0100

    Bump Version

--- java/org/silvercatcher/reforged/ReforgedMod.java
+++ java/org/silvercatcher/reforged/ReforgedMod.java
@@ -16,7 +16,7 @@ public class ReforgedMod
 {
 	public static final String NAME = "Reforged";
     public static final String ID = "reforged";
-    public static final String VERSION = "0.6-pre2";
+    public static final String VERSION = "0.6";
     /**Value depends on Download location... If the update is only released
     on GitHub, then set this to true
     If it gets released on CurseForge as well, then set it to false*/
