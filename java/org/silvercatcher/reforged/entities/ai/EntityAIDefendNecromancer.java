e0bf029b5d4f332d9e4ce2eebf2c519c139bc854
TheOnlySilverClaw <TheOnlySilverClaw@users.noreply.github.com>
2016-03-05 15:30:30 +0100

    new system for tracking enslaved entities

--- java/org/silvercatcher/reforged/entities/ai/EntityAIDefendNecromancer.java
+++ java/org/silvercatcher/reforged/entities/ai/EntityAIDefendNecromancer.java
@@ -11,17 +11,18 @@ import net.minecraft.entity.ai.EntityAIOwnerHurtTarget;
 import net.minecraft.entity.ai.EntityAITarget;
 import net.minecraft.entity.monster.EntitySkeleton;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.pathfinding.PathNavigate;
 
 public class EntityAIDefendNecromancer extends EntityAIBase {
 
 	
 	protected final EntityPlayer master;
 	protected final EntityCreature slave;
-	
+		
 	protected final int updateInterval;
 	protected final double attackSpeed;
 	
-	//protected EntityLivingBase target;
+	protected EntityLivingBase target;
 	protected int updateCountdown;
 	
 	
@@ -33,7 +34,7 @@ public class EntityAIDefendNecromancer extends EntityAIBase {
 	public EntityAIDefendNecromancer(EntityPlayer master, EntityCreature slave,
 			int updateInterval, double attackSpeed) {
 		
-		setMutexBits(1);
+		setMutexBits(3);
 		
 		this.master = Objects.requireNonNull(master, "Master must be defined!");
 		this.slave = Objects.requireNonNull(slave, "Slave must be defined!");
@@ -45,20 +46,32 @@ public class EntityAIDefendNecromancer extends EntityAIBase {
 	@Override
 	public boolean shouldExecute() {
 		
-		return slave.getAttackTarget() != master.getLastAttacker()
-				&& shouldBeAttacked(master.getLastAttacker());
+		return target != null && target.isEntityAlive() && slave.canEntityBeSeen(target);
 	}
 	
 	
 	@Override
 	public void startExecuting() {
 		
-		slave.setAttackTarget(master.getLastAttacker());
+		slave.setAttackTarget(target);
+		slave.setRevengeTarget(target);
+		
+		PathNavigate navigator = slave.getNavigator();
+		
+		if(navigator.noPath() && updateCountdown-- == 0) {
+			
+			updateCountdown = updateInterval;
+			navigator.tryMoveToEntityLiving(target, attackSpeed);
+			System.out.println(navigator.getPath());
+		}
 	}
 	
 	
-	private boolean shouldBeAttacked(EntityLivingBase target) {
+	public void setTarget(EntityLivingBase target) {
 		
-		return target != null && target != slave && target.isEntityAlive();
+		if(target == master || target == slave) {
+			target = null;
+		}
+		this.target = target;
 	}
 }
--- java/org/silvercatcher/reforged/items/weapons/ItemNecromancersStaff.java
+++ java/org/silvercatcher/reforged/items/weapons/ItemNecromancersStaff.java
@@ -1,7 +1,10 @@
 package org.silvercatcher.reforged.items.weapons;
 
+import java.util.Arrays;
+
 import org.silvercatcher.reforged.entities.ai.EntityAIDefendNecromancer;
 import org.silvercatcher.reforged.entities.ai.EntityAIFollowNecromancer;
+import org.silvercatcher.reforged.items.CompoundTags;
 import org.silvercatcher.reforged.items.ExtendedItem;
 
 import net.minecraft.entity.Entity;
@@ -9,15 +12,18 @@ import net.minecraft.entity.EntityCreature;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.ai.EntityAIAttackOnCollide;
+import net.minecraft.entity.ai.EntityAIBase;
 import net.minecraft.entity.ai.EntityAIControlledByPlayer;
 import net.minecraft.entity.ai.EntityAIFollowOwner;
 import net.minecraft.entity.ai.EntityAISwimming;
 import net.minecraft.entity.monster.EntityZombie;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 
 public class ItemNecromancersStaff extends ExtendedItem {
 
+	
 	public ItemNecromancersStaff() {
 		
 		setUnlocalizedName("necromancers_staff");
@@ -34,21 +40,46 @@ public class ItemNecromancersStaff extends ExtendedItem {
 	
 	@Override
 	public boolean onLeftClickEntity(ItemStack stack, EntityPlayer player, Entity entity) {
-		
+				
 		if(entity instanceof EntityCreature) {
 			
 			EntityCreature creature = (EntityCreature) entity;
 			
-			// empty brain
-			creature.tasks.taskEntries.clear();
-			creature.targetTasks.taskEntries.clear();
+			NBTTagCompound compound = CompoundTags.giveCompound(stack);
 			
-			creature.targetTasks.addTask(0, new EntityAIDefendNecromancer(player, creature));
-			creature.targetTasks.addTask(1, new EntityAIFollowNecromancer(player, creature));
 			
-			creature.tasks.addTask(2, new EntityAIAttackOnCollide(creature, 1, false));
-			creature.tasks.addTask(3, new EntityAISwimming(creature));
+			int [] slaveIDs;
+			
+			if(compound.hasKey("slaves")) {
+				slaveIDs = compound.getIntArray("slaves");
+			} else {
+				slaveIDs = new int [4];
+				compound.setIntArray("slaves", slaveIDs);
+			}
+			
+			if(isSlave(slaveIDs, creature)) {
+				
+				// some reaction
+			} else {
+				
+				enslave(slaveIDs, creature);
+			}
 		}
 		return true;
+	}
+
+	private boolean isSlave(int [] slaveIDs, EntityCreature creature) {
+		
+		
+		int creatureID = creature.getEntityId();
+		return 0 < Arrays.binarySearch(slaveIDs, creatureID);
+	}
+	
+	
+	private void enslave(int [] slaveIDs, EntityCreature creature) {
+
+		slaveIDs[0] = creature.getEntityId();
+		Arrays.sort(slaveIDs);
+		System.out.println(Arrays.toString(slaveIDs));
 	}
 }
