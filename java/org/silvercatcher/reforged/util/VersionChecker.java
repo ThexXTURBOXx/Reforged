7990ba5931d2fb19657b575bc6cd0b14395c3337
ThexXTURBOXx <nico.mexis@kabelmail.de>
2016-03-16 23:02:18 +0100

    New and improved Version Checker

--- java/org/silvercatcher/reforged/util/VersionChecker.java
+++ java/org/silvercatcher/reforged/util/VersionChecker.java
@@ -2,17 +2,31 @@ package org.silvercatcher.reforged.util;
 
 import org.silvercatcher.reforged.ReforgedMod;
 
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.event.FMLInterModComms;
 
 public class VersionChecker extends Thread {
 	
-	public static final String link = "https://raw.githubusercontent.com/TheOnlySilverClaw/Reforged/master/versionchecker.json";
+	public static final String curseID = "241392-reforged";
+	public static final String curseIntegrationID = "242591-reforged-integration";
 	
 	@Override
 	public void run() {
 		if(Loader.isModLoaded("VersionChecker")) {
-			FMLInterModComms.sendRuntimeMessage(ReforgedMod.ID, "VersionChecker", "addVersionCheck", link);
+			NBTTagCompound cpd = new NBTTagCompound();
+			cpd.setString("curseProjectName", curseID);
+			cpd.setString("curseFilenameParser", "Reforged []-1.8.9");
+			cpd.setString("modDisplayName", ReforgedMod.NAME);
+			cpd.setString("oldVersion", ReforgedMod.VERSION);
+			FMLInterModComms.sendRuntimeMessage(ReforgedMod.ID, "VersionChecker", "addCurseCheck", cpd);
+			if(Loader.isModLoaded("ReforgedIntegrationCore")) {
+				NBTTagCompound cpd1 = new NBTTagCompound();
+				cpd1.setString("curseProjectName", curseIntegrationID);
+				cpd1.setString("curseFilenameParser", "1.8.9 Reforged Integration []");
+				cpd1.setString("modDisplayName", "Reforged Integration");
+				FMLInterModComms.sendRuntimeMessage("ReforgedIntegrationCore", "VersionChecker", "addCurseCheck", cpd1);				
+			}
 		}
 	}
 	
