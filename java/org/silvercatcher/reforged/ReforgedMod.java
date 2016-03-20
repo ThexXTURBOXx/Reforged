52fb75f9c00eb08f46a0b4c71605c16592f7ce5b
ThexXTURBOXx <nico.mexis@kabelmail.de>
2016-03-20 11:48:04 +0100

    We don't need this anymore

--- java/org/silvercatcher/reforged/ReforgedMod.java
+++ java/org/silvercatcher/reforged/ReforgedMod.java
@@ -17,10 +17,6 @@ public class ReforgedMod
 	public static final String NAME = "Reforged";
     public static final String ID = "reforged";
     public static final String VERSION = "0.6.1";
-    /**Value depends on Download location... If the update is only released
-    on GitHub, then set this to true
-    If it gets released on CurseForge as well, then set it to false*/
-    public static final boolean BETA = false;
     
     public static final CreativeTabs tabReforged = new CreativeTabs(ID) {
 		@Override
