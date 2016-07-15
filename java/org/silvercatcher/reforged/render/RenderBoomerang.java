b442a5213739f0ff60472e514c21e3a647bfdcb2
ThexXTURBOXx <nico.mexis@kabelmail.de>
2016-07-15 14:59:19 +0200

    Boomerang FIx

--- java/org/silvercatcher/reforged/render/RenderBoomerang.java
+++ java/org/silvercatcher/reforged/render/RenderBoomerang.java
@@ -6,9 +6,12 @@ import org.silvercatcher.reforged.entities.EntityBoomerang;
 import org.silvercatcher.reforged.material.MaterialManager;
 import org.silvercatcher.reforged.models.ModelBoomerang;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.Vec3;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -27,8 +30,26 @@ public class RenderBoomerang extends ReforgedRender {
 		bindTexture(getEntityTexture(theEntity));
 		GL11.glTranslated(x, y, z);
 		GL11.glScalef(scale, scale, scale);
+		int boomerangCount = 1;
+		for(Object o : Minecraft.getMinecraft().theWorld.loadedEntityList) {
+			if(o instanceof EntityBoomerang) {
+				EntityPlayer player = Minecraft.getMinecraft().thePlayer;
+				EntityBoomerang boomerang = (EntityBoomerang) o;
+				Vec3 look = player.getLookVec();
+				Vec3 boomerangvec = new Vec3(boomerang.posX - player.posX,
+						(boomerang.getEntityBoundingBox().minY + boomerang.height / 2)
+						- player.posY + player.getEyeHeight(),
+						boomerang.posZ - player.posZ);
+				double d0 = boomerangvec.lengthVector();
+				double d1 = look.dotProduct(boomerangvec);
+				boolean seen = d1 > 1 - 0.25 / d0;
+				if(seen && Minecraft.getMinecraft().thePlayer.canEntityBeSeen(boomerang)) {
+					boomerangCount++;
+				}
+			}
+		}
 		//If the number is set higher, then the Boomerang will rotate faster, otherwise slower
-		yawb += 5;
+		yawb += ((double) 5 / boomerangCount);
 		GL11.glRotated(yawb, 0, 1, 0);
 		model.render(theEntity,(float) x,(float) y,(float) z, yawb, partialTick, 0.0475F);
 		GL11.glPopMatrix();
