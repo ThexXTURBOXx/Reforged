fd21b18dd199c4e8b2306d959b7a7bc9036c61b4
Nico <nico.mexis@kabelmail.de>
2016-11-19 18:43:21 +0100

    Bump Version

--- java/org/silvercatcher/reforged/ReforgedMod.java
+++ java/org/silvercatcher/reforged/ReforgedMod.java
@@ -18,7 +18,7 @@ public class ReforgedMod {
 	
 	public static final String NAME = "Reforged";
 	public static final String ID = "reforged";
-	public static final String VERSION = "0.7-pre4";
+	public static final String VERSION = "0.7";
 	
 	public static boolean battlegearDetected;
 	
