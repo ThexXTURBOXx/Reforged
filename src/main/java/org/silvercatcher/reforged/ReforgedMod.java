b0d898f0b459303b412558f43ead32bb615867e0
Nico Mexis <nico.mexis@kabelmail.de>
2022-09-03 17:32:55 +0200

    Bump version

--- src/main/java/org/silvercatcher/reforged/ReforgedMod.java
+++ src/main/java/org/silvercatcher/reforged/ReforgedMod.java
@@ -24,7 +24,7 @@ public class ReforgedMod {
 
     public static final String NAME = "Reforged";
     public static final String ID = "reforged";
-    public static final String VERSION = "0.7.6";
+    public static final String VERSION = "0.7.7";
     public static final String UPDATE_JSON = "https://raw.githubusercontent"
             + ".com/ThexXTURBOXx/UpdateJSONs/master/reforged.json";
 
@@ -47,8 +47,8 @@ public class ReforgedMod {
     @Instance(ID)
     public static ReforgedMod instance;
 
-    @SidedProxy(modId = ID, clientSide = "org.silvercatcher.reforged.proxy.ClientProxy", serverSide = "org"
-            + ".silvercatcher.reforged.proxy.CommonProxy")
+    @SidedProxy(modId = ID, clientSide = "org.silvercatcher.reforged.proxy.ClientProxy",
+            serverSide = "org.silvercatcher.reforged.proxy.CommonProxy")
     public static CommonProxy proxy;
 
     @EventHandler
