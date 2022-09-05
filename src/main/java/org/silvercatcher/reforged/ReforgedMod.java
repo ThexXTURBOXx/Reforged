c130739b432e6ec780582050846066f5f946745a
Nico Mexis <nico.mexis@kabelmail.de>
2022-09-05 18:41:56 +0200

    Bump version

--- src/main/java/org/silvercatcher/reforged/ReforgedMod.java
+++ src/main/java/org/silvercatcher/reforged/ReforgedMod.java
@@ -24,7 +24,7 @@ public class ReforgedMod {
 
     public static final String NAME = "Reforged";
     public static final String ID = "reforged";
-    public static final String VERSION = "0.7.7";
+    public static final String VERSION = "0.7.8";
     public static final String UPDATE_JSON = "https://raw.githubusercontent"
             + ".com/ThexXTURBOXx/UpdateJSONs/master/reforged.json";
 
